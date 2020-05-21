c2ca52fb9c4084732dee908301fc1d2879014201
Ana Sundji <as49180@fer.hr>
2020-05-21 16:45:06 +0200

    Napravljen glavni izbornik i custom particle emitter.

---
+++ Morbus/Assets/Scripts/HomeUserInterfaceManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeUserInterfaceManager : MonoBehaviour
+{
+
+    public GameObject PanelMenu;
+    public GameObject PanelHowToPlay;
+    public GameObject PanelSettings;
+
+    private void Awake()
+    {
+        PanelMenu.SetActive(true);
+        PanelHowToPlay.SetActive(false);
+        PanelSettings.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        // Resetirati sve podatke u GameManager-u (za svaki slučaj) i pokrenuti scenu s
+        // indeksom 1.
+    }
+
+    public void LoadGame()
+    {
+        // Otvoriti izbornik za izbor save-a ili odmah dohvatiti podatke u GameManager-u
+        // i pokrenuti odgovarajuću scenu.
+    }
+
+    public void OpenHowToPlay()
+    {
+        PanelMenu.SetActive(false);
+        PanelHowToPlay.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        PanelMenu.SetActive(false);
+        PanelSettings.SetActive(true);
+    }
+
+    public void Return()
+    {
+        PanelMenu.SetActive(true);
+        PanelHowToPlay.SetActive(false);
+        PanelSettings.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Morbus/Assets/Scripts/ParticleEmitter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmitter : MonoBehaviour
+{
+
+    [Header("Particle Details")]
+
+    public GameObject ParticleObject;
+    public int ParticlesInSceneLimit;
+
+    [Header("Emission Details")]
+
+    public Vector2 DirectionRangeX;
+    public Vector2 DirectionRangeY;
+    public Vector2 DirectionRangeZ;
+    public Vector2 FrequencyRange;
+    public Vector2 ScaleRange;
+    public Vector2 SpeedRange;
+    public Vector2 StartPointRangeX;
+    public Vector2 StartPointRangeY;
+    public Vector2 StartPointRangeZ;
+
+    private int _particles;
+    private float _timer;
+    private Transform _transform;
+
+    private void Awake()
+    {
+
+        _transform = transform;
+        ParticleBehaviour.ParticleCreatedEvent.AddListener(ParticleNumberIncrease);
+        ParticleBehaviour.ParticleDestroyedEvent.AddListener(ParticleNumberDecrease);
+
+        CreateParticle();
+        CreateParticle();
+        CreateParticle();
+
+    }
+
+    private void Update()
+    {
+
+        _timer -= Time.deltaTime;
+
+        if (_timer < 0 && _particles < ParticlesInSceneLimit)
+        {
+            _timer = FrequencyRange.RandomValue();
+            CreateParticle();
+        }
+
+    }
+
+    private void CreateParticle()
+    {
+
+        Vector3 direction = new Vector3(DirectionRangeX.RandomValue(), DirectionRangeY.RandomValue(), DirectionRangeZ.RandomValue());
+        Vector3 position = new Vector3(StartPointRangeX.RandomValue(), StartPointRangeY.RandomValue(), StartPointRangeZ.RandomValue());
+
+        GameObject spawnedParticleObject = Instantiate(ParticleObject, _transform);
+        spawnedParticleObject.GetComponent<ParticleBehaviour>().SetUpTransform(position, ScaleRange.RandomValue());
+        spawnedParticleObject.GetComponent<ParticleBehaviour>().SetUpRigidbody(direction.normalized, SpeedRange.RandomValue());
+
+    }
+
+    private void ParticleNumberDecrease()
+    {
+        _particles -= 1;
+    }
+
+    private void ParticleNumberIncrease()
+    {
+        _particles += 1;
+    }
+
+}
