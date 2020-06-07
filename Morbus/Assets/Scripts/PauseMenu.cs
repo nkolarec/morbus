0d6495e84b63a5913e266db2075b93a6b6ae3ff9
Lorena Bastalić <lb49581@fer.hr>
2020-06-07 23:40:23 +0200

    dodan Pause Menu

---
+++ Morbus/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool isPaused = false;
+    public static bool isGamePaused;
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+            Time.timeScale = 0;
+            isPaused = true;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMenu() 
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Home");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
